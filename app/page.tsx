015602d6b926ea759ae5fff6ef8ff19b858832e0
jpalvarado101 <jpalvarado101@icloud.com>
2025-01-03 19:11:59 -0800

    hero page almost done

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero />
-      </div>
+    <main className="relative flex flex-col overflow-hidden w-full">
+      {/* Hero Section - Black Background */}
+      <section className="flex justify-center items-center bg-black-100 text-white min-h-screen w-full">
+        <div className="w-full">
+          <Hero />
+        </div>
+      </section>
+
+      {/* Orange Section */}
+      <section className="flex justify-center items-center !bg-orange-500 min-h-screen w-full">
+        <div className="w-full">
+          <h1 className="text-3xl font-bold">Orange Section</h1>
+        </div>
+      </section>
+
+      {/* White Section */}
+      <section className="flex justify-center items-center bg-white min-h-screen w-full">
+        <div className="w-full">
+          <h1 className="text-3xl font-bold">White Section</h1>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { FlipWords } from "./ui/flip-words";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const Hero = () => {
-  const words = ["better", "cute", "beautiful", "modern"];
+  const words = ["smart", "cute", "beautiful", "modern"];
   return (
     <div className="pb-20 pt-36">
       <div>
@@ -25,10 +26,22 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89w] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+          <h2 className="uppercase tracking-widest text-small text-center text-blue-100 max-w-80">
             Hi World!
           </h2>
-          <FlipWords words={words} className="text-small" /> <br />
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Hi my name is John"
+          />
+          <FlipWords
+            className="text-regular text-center md:text-5xl lg:text-6xl"
+            words={words}
+            duration={100}
+          />
+          <br />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I am John, a computer engineer based in Canada.
+          </p>
         </div>
       </div>
     </div>
